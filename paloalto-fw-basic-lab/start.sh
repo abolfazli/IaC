ea3086edd1dcf166d813758c1305d126d10d6c7d
Amir Abolfazli <abolfazli@outlook.com>
2021-12-07 17:22:55 -0500

    Update start.sh

--- paloalto-fw-basic-lab/start.sh
+++ paloalto-fw-basic-lab/start.sh
@@ -16,3 +16,26 @@ sleep 60s
 
 terraform init
 terraform apply -auto-approve
+
+export FIREWALL_MGMT_EXIP=$(gcloud compute instances describe pa-firewall --zone=us-central1-a --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+
+echo "Firewall ip address is: $FIREWALL_MGMT_EXIP 
+
+You can now close this cloud shell if you know what to do. If you are new stay with me some more minutes. 
+We are going to SSH to this device after 5 minutes when firewall completely boots up and the authentication module become ready. 
+Please define a complex password for admin user.
+
+===================================================================
+Enter the following commands as you are connected to the firewall.
+> configure
+# set mgt-config users admin password
+Then Enter your password and enter \"commit\" and exit the cli and connect to firewall using URL below:
+===================================================================
+
+https://$FIREWALL_MGMT_EXIP
+
+
+Have fun"
+
+sleep 5m
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/id_rsa admin@$FIREWALL_MGMT_EXIP
